1bc6a6307f713c977946ed041c3188cf72a018b7
Max Abzaloff <max.abzaloff@gmail.com>
2021-05-09 09:45:05 +1000

    :boom: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rules: {
+        strict: 2,
+    },
+    env: {
+        browser: true,
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+}
